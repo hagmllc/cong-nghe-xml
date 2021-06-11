2c08b746ba6a142cb2f050be7e341a61e0a3ce8f
Phạm Hải Anh <63529879+hagmllc@users.noreply.github.com>
2021-06-11 14:21:33 +0700

    Initial commit

---
+++ PhamHaiAnh_2018600641/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace PhamHaiAnh_2018600641
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_manv = new System.Windows.Forms.TextBox();
+            this.txt_hoten = new System.Windows.Forms.TextBox();
+            this.txt_dienthoai = new System.Windows.Forms.TextBox();
+            this.txt_tendonvi = new System.Windows.Forms.TextBox();
+            this.txt_timkiem = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.manv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dienthoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tendonvi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(393, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhân viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(70, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã nhân viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Họ tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Điện thoại";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(70, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Tên đơn vị";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(70, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Nhập nội dung tìm kiếm";
+            // 
+            // txt_manv
+            // 
+            this.txt_manv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_manv.Location = new System.Drawing.Point(337, 130);
+            this.txt_manv.Name = "txt_manv";
+            this.txt_manv.Size = new System.Drawing.Size(270, 27);
+            this.txt_manv.TabIndex = 2;
+            // 
+            // txt_hoten
+            // 
+            this.txt_hoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_hoten.Location = new System.Drawing.Point(337, 167);
+            this.txt_hoten.Name = "txt_hoten";
+            this.txt_hoten.Size = new System.Drawing.Size(270, 27);
+            this.txt_hoten.TabIndex = 2;
+            // 
+            // txt_dienthoai
+            // 
+            this.txt_dienthoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_dienthoai.Location = new System.Drawing.Point(337, 204);
+            this.txt_dienthoai.Name = "txt_dienthoai";
+            this.txt_dienthoai.Size = new System.Drawing.Size(270, 27);
+            this.txt_dienthoai.TabIndex = 2;
+            // 
+            // txt_tendonvi
+            // 
+            this.txt_tendonvi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tendonvi.Location = new System.Drawing.Point(337, 241);
+            this.txt_tendonvi.Name = "txt_tendonvi";
+            this.txt_tendonvi.Size = new System.Drawing.Size(270, 27);
+            this.txt_tendonvi.TabIndex = 2;
+            // 
+            // txt_timkiem
+            // 
+            this.txt_timkiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_timkiem.Location = new System.Drawing.Point(337, 278);
+            this.txt_timkiem.Name = "txt_timkiem";
+            this.txt_timkiem.Size = new System.Drawing.Size(270, 27);
+            this.txt_timkiem.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(753, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(753, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Xoá";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(753, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tìm kiếm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.manv,
+            this.hoten,
+            this.dienthoai,
+            this.tendonvi});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 335);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(912, 320);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // manv
+            // 
+            this.manv.HeaderText = "Mã nhân viên";
+            this.manv.MinimumWidth = 6;
+            this.manv.Name = "manv";
+            // 
+            // hoten
+            // 
+            this.hoten.HeaderText = "Họ tên";
+            this.hoten.MinimumWidth = 6;
+            this.hoten.Name = "hoten";
+            // 
+            // dienthoai
+            // 
+            this.dienthoai.HeaderText = "Điện thoại";
+            this.dienthoai.MinimumWidth = 6;
+            this.dienthoai.Name = "dienthoai";
+            // 
+            // tendonvi
+            // 
+            this.tendonvi.HeaderText = "Tên đơn vị";
+            this.tendonvi.MinimumWidth = 6;
+            this.tendonvi.Name = "tendonvi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 680);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_timkiem);
+            this.Controls.Add(this.txt_tendonvi);
+            this.Controls.Add(this.txt_dienthoai);
+            this.Controls.Add(this.txt_hoten);
+            this.Controls.Add(this.txt_manv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PhamHaiAnh_2018600641";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_manv;
+        private System.Windows.Forms.TextBox txt_hoten;
+        private System.Windows.Forms.TextBox txt_dienthoai;
+        private System.Windows.Forms.TextBox txt_tendonvi;
+        private System.Windows.Forms.TextBox txt_timkiem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn manv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dienthoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tendonvi;
+    }
+}
+
---
+++ PhamHaiAnh_2018600641/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PhamHaiAnh_2018600641
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument doc = new XmlDocument();
+        string path = @"C:\Users\haian\Desktop\PhamHaiAnh_2018600641\PhamHaiAnh_2018600641\nhanvien.xml";
+        int d;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hienthi();
+        }
+        private void hienthi()
+        {
+            dataGridView1.Rows.Clear();
+            doc.Load(path);
+            XmlNodeList ds = doc.SelectNodes("/congty/nhanvien");
+            int sd = 0;
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Rows.Add();
+            foreach (XmlNode nv in ds)
+            {
+                XmlNode manv = nv.SelectSingleNode("manv");
+                dataGridView1.Rows[sd].Cells[0].Value = manv.InnerText.ToString();
+                XmlNode hoten = nv.SelectSingleNode("hoten");
+                dataGridView1.Rows[sd].Cells[1].Value = hoten.InnerText.ToString();
+                XmlNode donvi = nv.SelectSingleNode("donvi");
+                XmlNode dienthoai = donvi.SelectSingleNode("@dienthoai");
+                dataGridView1.Rows[sd].Cells[2].Value = dienthoai.InnerText.ToString();
+                XmlNode tendonvi = donvi.SelectSingleNode("tendonvi");
+                dataGridView1.Rows[sd].Cells[3].Value = tendonvi.InnerText.ToString();
+                dataGridView1.Rows.Add();
+                sd++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doc.Load(path);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode matrung = doc.SelectSingleNode("/congty/nhanvien[manv='" + txt_manv.Text + "']");
+            if (matrung != null)
+            {
+                MessageBox.Show("Đã có mã nhân viên");
+            }
+            else
+            {
+                XmlNode nv = doc.CreateElement("nhanvien");
+
+                XmlNode manv = doc.CreateElement("manv");
+                manv.InnerText = txt_manv.Text;
+                nv.AppendChild(manv);
+                //họ tên
+                XmlNode hoten = doc.CreateElement("hoten");
+                hoten.InnerText = txt_hoten.Text;
+                nv.AppendChild(hoten);
+                //dơn vị
+                XmlNode donvi = doc.CreateElement("donvi");
+
+                XmlAttribute dienthoai = doc.CreateAttribute("dienthoai");
+                dienthoai.InnerText = txt_dienthoai.Text;
+                donvi.Attributes.Append(dienthoai);
+
+                XmlNode tendonvi = doc.CreateElement("tendonvi");
+                tendonvi.InnerText = txt_tendonvi.Text;
+                donvi.AppendChild(tendonvi);
+
+                nv.AppendChild(donvi);
+                goc.AppendChild(nv);
+            }
+            
+
+            doc.Save(path);
+            hienthi();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            doc.Load(path);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode nvXoa = goc.SelectSingleNode("/congty/nhanvien[manv='" + txt_manv.Text + "']");
+            MessageBox.Show("Bạn có muốn xoá không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            goc.RemoveChild(nvXoa);
+            doc.Save(path);
+            hienthi();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            doc.Load(path);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode nv = goc.SelectSingleNode("/congty/nhanvien/donvi[tendonvi='" + txt_timkiem.Text + "']");
+
+            if (txt_timkiem.TextLength == 0)
+            {
+                MessageBox.Show("Ban chua nhap tu tim kiem");
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.ColumnCount = 4;
+                dataGridView1.Rows.Add();
+                XmlNode manv = nv.SelectSingleNode("manv");
+                dataGridView1.Rows[0].Cells[0].Value = manv.InnerText.ToString();
+                XmlNode hoten = nv.SelectSingleNode("hoten");
+                dataGridView1.Rows[0].Cells[1].Value = hoten.InnerText.ToString();
+                XmlNode donvi = nv.SelectSingleNode("donvi");
+                XmlNode dienthoai = donvi.SelectSingleNode("@dienthoai");
+                dataGridView1.Rows[0].Cells[2].Value = dienthoai.InnerText.ToString();
+                XmlNode tendonvi = donvi.SelectSingleNode("tendonvi");
+                dataGridView1.Rows[0].Cells[3].Value = tendonvi.InnerText.ToString();
+                dataGridView1.Rows.Add();
+
+            }
+        }
+    }
+}
